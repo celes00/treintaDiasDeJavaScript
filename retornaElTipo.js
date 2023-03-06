466dab44445089dfba36692a2907c1d37f2ac415
Maria Celeste Antelo <103297382+celes00@users.noreply.github.com>
2023-03-06 19:56:49 -0300

    creo la funcion de retornaElTipo

---
+++ retornaElTipo.js
@@ -0,0 +1,5 @@
+//Consigna hacer una funcion llamada solution que recibe un parametro llamado valor. Debes encontrar el tipo de dato del parametro valor y retornarlo desde la funcion solution.
+// Recuerda que el paraetro valor sera distitno por cada distinta fomra en que ejecutemos la funcion solution. 
+function solution(valor) {
+    return typeof valor
+}
